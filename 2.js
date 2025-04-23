cc6078ace9a10ddd9653cd659b37db26be280a9d
hieu12x <dohieu1025@gmail.com>
2025-04-23 11:05:36 +0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,7 @@
+// var username = prompt('Nhập tài khoản')
+// if(username.lenght == 0)
+// {
+//     console.error("Ko duoc bo trong")
+// }
+ var string = "Tối rồi mà vẫn phải học"
+ console.log(str.substring(0,3))
