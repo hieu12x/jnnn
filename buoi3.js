5fb017ec8d7eedf1464f3a3a49746312d25dce46
hieu12x <dohieu1025@gmail.com>
2025-05-01 19:41:47 +0700

    Add files via upload

---
+++ buoi3.js
@@ -0,0 +1,9 @@
+var quaTang = ['Son','Tien','GauBong']
+// quaTang.unshift['Phan'] //them vao dau`
+// quaTang.push('Kem nen')
+// quaTang.shift()
+// var gift = quaTang.pop()
+// console.log(quaTang)
+// quaTang.splice(1,1,'Vay')
+// console.log(quaTang)
+console.log(quaTang.indexOf('Tien'))
