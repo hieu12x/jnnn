533d3b67ef8a8e7f1b85e3163f5bc43f645934dd
hieu12x <dohieu1025@gmail.com>
2025-06-06 23:56:22 +0700

    Add files via upload

---
+++ ham`.js
@@ -0,0 +1,12 @@
+function tinhTong(a,b)
+{
+    var c = a * b
+    return c
+}
+//html
+<button onclick="truoc"()></button>
+<button onclick="sau"()></button>
+<img
+//js
+funtuon truoc(){
+    document.writeln('<img src')'
