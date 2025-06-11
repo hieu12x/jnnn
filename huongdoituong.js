2d6ea8fd04d9854fa40375df9909ee7850df6216
hieu12x <dohieu1025@gmail.com>
2025-06-11 23:07:31 +0700

    Add files via upload

---
+++ huongdoituong.js
@@ -0,0 +1,23 @@
+var arr = [{
+    ten: 'Minh',
+    tuoi: 24,
+    ngheNghiep: "Giang Vien"
+    diaChi: "Bac Giang",
+},
+{
+   ten: Ngoc
+   tuoi
+
+
+}
+{
+    ten 
+    tuoi
+}
+for(let i = 0; i < arr.length; i++)
+{
+    if(arr[i].tuoi >= 22)
+    console.log(arr[i])
+console.log(arr[i].ten + 'ra trường')
+
+}
