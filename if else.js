90c09525531a081b2493c145244f324eb5978d03
hieu12x <dohieu1025@gmail.com>
2025-06-12 23:05:44 +0700

    Add files via upload

---
+++ if else.js
@@ -0,0 +1,30 @@
+// var taiSan = prompt("nhap vao san cua ban")
+// if (taiSan > 1000000000) {
+//     console.log("Ty Phu")
+// }
+// el if (taiSan > 1000000) {
+//     console.log("Tieu Phu")
+// }
+// if (taiSan < 1000000) {
+//     console.log("Nguoi Thuong")
+// }
+var thoiGian = Number(prompt('xin hay nhap vao thoi gian'))
+
+switch(thoiGian){
+    case 7 : console.log('Ngu?')
+    case 13 : console.log('thuc day0')
+    case 14 : console.log('an chinh')
+    case 18 : console.log('Di tam')
+    case 19 : console.log('ve nha')
+}
+let i = 0
+while(i<10)
+{
+    console.log(i)
+    i++
+}
+ var crush =["dieu", "tram anh", "trang"]
+ for(let crush of crushes)
+ {
+    console.log(crush)
+ }
