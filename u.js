e36fe1c7c7b4e19495919a1c7efd44651c1ea586
hieu12x <dohieu1025@gmail.com>
2025-07-13 22:10:23 +0700

    Add files via upload

---
+++ u.js
@@ -0,0 +1,12 @@
+console.log(document.getElementById)
+function doiMau() {
+
+}
+function valForm()
+{
+    if((document.getElementById('hoTen').value) == "") {
+        document.getElementById("err=ten").innerHTML = "ko hop le"
+        return false
+
+    }
+}
